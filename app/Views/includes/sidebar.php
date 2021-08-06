eb80e34914a5e91337129de84aa088825f40956b
benacq <benacq44@gmail.com>
2021-08-06 23:05:42 +0000

    sidebar icons

--- app/Views/includes/sidebar.php
+++ app/Views/includes/sidebar.php
@@ -1,33 +1,33 @@
-
 <section id="flex-child-sidebar">
-<div class="dashboard_sidebar">
-<h3 class ="dashboard_logo">C.I.S</h3><br/>
+    <div class="dashboard_sidebar">
+        <h3 class="dashboard_logo">C.I.S</h3><br/>
 
-<div class="dashboard_sidebar_user">
- <img src="style.jpg" alt="image" width="100px" height="100px" class="center"/>
- <span> CLAUDIA</span>
-  </div>
+        <div class="dashboard_sidebar_user">
+            <img src="style.jpg" alt="image" width="100px" height="100px" class="center"/>
+            <span> CLAUDIA</span>
+        </div>
 
-  <div class="dashboard_sidebar_menus"> 
-    <ul class="dashboard-menu-lists-wrapper">
-       <li>
-            <a class="side-bar-menu-link" href="<?= URL_ROOT ?>/pages/dashboard"><i class="fa fa-dashboard"></i> &nbsp;
-                Dashboard</a>
-        </li>
-        <li><a class="side-bar-menu-link" href="editProfile.html"><i class="fa fa-user"></i> &nbsp;Edit Profile</a></li>
-        <li><a class="side-bar-menu-link" href="<?= URL_ROOT ?>/pages/suppliers"><i class="fa fa-group "></i> &nbsp;Suppliers</a>
-        </li>
-        <li><a class="side-bar-menu-link" href="employee.html"><i class="fa fa-users"></i> &nbsp;Employees</a></li>
-        <li><a class="side-bar-menu-link" href="<?= URL_ROOT ?>/pages/products"><i class="fa fa-sitemap"></i> &nbsp;Products</a><br/>
-        </li>
-        <li><a class="side-bar-menu-link" href="order.html"><i class="fa fa-shopping-cart"></i>&nbsp;Orders</a></li>
-        <li><a class="side-bar-menu-link" href="payment.html"><i class="fa fa-money"></i>&nbsp;Payment</a></li>
+        <div class="dashboard_sidebar_menus">
+            <ul class="dashboard-menu-lists-wrapper">
+                <li>
+                    <a class="side-bar-menu-link" href="<?= URL_ROOT ?>/pages/dashboard"><i class="fa fa-dashboard"></i>Dashboard</a>
+                </li>
+                <li><a class="side-bar-menu-link" href="editProfile.html"><i class="fa fa-user"></i>Edit Profile</a>
+                </li>
+                <li><a class="side-bar-menu-link" href="<?= URL_ROOT ?>/pages/suppliers"><i class="fa fa-group"></i>Suppliers</a>
+                </li>
+                <li><a class="side-bar-menu-link" href="employee.html"><i class="fa fa-users"></i>Employees</a>
+                </li>
+                <li><a class="side-bar-menu-link" href="<?= URL_ROOT ?>/pages/products"><i class="fa fa-sitemap"></i>Products</a><br/>
+                </li>
+                <li><a class="side-bar-menu-link" href="order.html"><i class="fa fa-shopping-cart"></i>Orders</a>
+                </li>
+                <li><a class="side-bar-menu-link" href="payment.html"><i class="fa fa-money"></i>Payment</a></li>
+            </ul>
 
-    </ul>
-    
             <p class="change-pass"><i class="fa fa-key"></i>CHANGE PASSWORD</p>
-            
- </div >
-</div> 
+
+        </div>
+    </div>
 
 </section>
